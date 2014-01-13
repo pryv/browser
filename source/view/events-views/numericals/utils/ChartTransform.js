89b4297c806a90d63e81064427f2b3a2f2c07849
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-01-13 14:57:55 +0100

    Changed interval mapper. Weekly mappings according to ISO8601's week numbering definition.

--- source/view/events-views/numericals/utils/ChartTransform.js
+++ source/view/events-views/numericals/utils/ChartTransform.js
@@ -1,51 +1,46 @@
-
 var _ = require('underscore');
 
 var ChartTransform = module.exports = {};
 
 /*
-TODO: special case transform:sum and interval:none continous sum per point
+ TODO: special case transform:sum and interval:none continous sum per point
  */
 
+
+/**
+ * ISO ISO8601 week numbers
+ * @returns {number} the week number of this date.
+ */
+
+Date.prototype.getISO8601Week = function () {
+  var target = new Date(this.valueOf());
+  var dayNr = (this.getDay() + 6) % 7;
+  target.setDate(target.getDate() - dayNr + 3);
+  var jan4 = new Date(target.getFullYear(), 0, 4);
+  var dayDiff = (target - jan4) / 86400000;
+  var weekNr = 1 + Math.ceil(dayDiff / 7);
+  return weekNr;
+};
+
+
 ChartTransform.transform = function (model) {
-  var i;
-
-  if (!model.get('transform') || !model.get('interval')) {
+  if (!model.get('transform')) {
     return this.default(model.get('events'));
   }
 
-  switch (model.get('interval')) {
-  case 'hourly' :
-    i = 3600;
-    break;
-  case 'daily' :
-    i = 24 * 3600;
-    break;
-  case 'weekly' :
-    i = 7 * 24 * 3600;
-    break;
-  case 'monthly' :
-    i = 30 * 7 * 24 * 3600;
-    break;
-  case 'yearly' :
-    i = 365 * 24 * 3600;
-    break;
-  default :
-    return this.default(model.get('events'));
-  }
-
-  var cut = this.intervalMapper(model.get('events'), i);
+  var f = this.getMapper(model.get('interval'));
+  var cut = this.map(model.get('events'), f);
 
   var r = null;
   switch (model.get('transform')) {
   case 'sum':
-    r =  this.sum(cut);
+    r = (f === null) ? this.stackedSum(cut) : this.sum(cut);
     break;
   case 'average':
-    r =  this.avg(cut);
+    r = this.avg(cut);
     break;
   default:
-    r =  this.default(model.get('events'));
+    r = this.default(model.get('events'));
     break;
   }
   return r;
@@ -53,25 +48,64 @@ ChartTransform.transform = function (model) {
 
 ChartTransform.default = function (data) {
   return _.map(data, function (e) {
-      return [e.time * 1000, +e.content];
-    });
+    return [e.time * 1000, +e.content];
+  });
 };
 
-ChartTransform.intervalMapper = function (data, i) {
-  var m = _.map(data, function (e) {
-    return [Math.floor(e.time / i), e.time * 1000, +e.content];
-  });
+ChartTransform.map = function (data, f) {
+  var m = null;
+  if (f === null) {
+    m = _.map(data, function (e) {
+      return [e.time * 1000, e.time * 1000, +e.content];
+    });
+  } else {
+    m = _.map(data, function (e) {
+      var d = new Date(e.time * 1000);
+      return [Math.floor(+f(d)), e.time * 1000, +e.content];
+    });
+  }
 
   return _.groupBy(m, function (e) {
     return e[0];
   });
 };
 
+ChartTransform.getMapper = function (interval) {
+  switch (interval) {
+  case 'hourly' :
+    return function (d) {
+      return d.getFullYear().toString() + d.getMonth().toString() +
+        d.getDate().toString() + d.getHours().toString();
+    };
+  case 'daily' :
+    return function (d) {
+      return d.getFullYear().toString() + d.getMonth().toString() + d.getDate().toString();
+    };
+  case 'weekly' :
+    return function (d) {
+      var msSinceFirstWeekday = d.getDay() * 24 * 3600 * 1000 +
+        d.getHours() * 3600 * 1000 + d.getMinutes() * 60 * 1000 + d.getMilliseconds();
+      var asWeek = new Date(d.getTime() - msSinceFirstWeekday);
+      return asWeek.getFullYear().toString() + asWeek.getISO8601Week().toString();
+    };
+  case 'monthly' :
+    return function (d) {
+      return d.getFullYear().toString() + d.getMonth().toString();
+    };
+  case 'yearly' :
+    return function (d) {
+      return d.getFullYear().toString();
+    };
+  default :
+    return null;
+  }
+};
+
 
 /*
-  in -> {1234: [123, 1234, 345], 145: [1234] ,...}
-  out -> [[1234, sum], [145, sum]]
-*/
+ in -> {1234: [123, 1234, 345], 145: [1234] ,...}
+ out -> [[1234, sum], [145, sum]]
+ */
 ChartTransform.sum = function (data) {
   var r = [];
   var summer = function (a) {
@@ -90,8 +124,40 @@ ChartTransform.sum = function (data) {
 
 /*
  in -> {1234: [123, 1234, 345], 145: [1234] ,...}
+ out -> [[1234, sum], [145, sum]]
+ */
+ChartTransform.stackedSum = function (data) {
+  var r = [];
+  var previous = 0;
+
+  var summer = function (a) {
+    return _.reduce(a, function (c, e) {
+      var s = c + e[2] + previous;
+      previous = s;
+      return s;
+    }, 0);
+  };
+
+  var keys = [];
+
+  for (var key in data) {
+    if (data.hasOwnProperty(key)) {
+      keys.push(key);
+    }
+  }
+  keys.sort();
+
+  for (var i = 0; i < keys.length; ++i) {
+    r.push([+keys[i], summer(data[keys[i]])]);
+  }
+  return r;
+};
+
+
+/*
+ in -> {1234: [123, 1234, 345], 145: [1234] ,...}
  out -> [[1234, avg], [145, avg]]
-*/
+ */
 ChartTransform.avg = function (data) {
   var r = [];
   var averager = function (a) {

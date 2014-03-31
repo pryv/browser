e0ab6dad5564c653781b2d0e6182aae6c90d8a5c
jpagand <jordane.pagand@epfl.ch>
2014-03-31 17:10:08 +0200

    Fixed bug with public sharing and relogging

--- source/Model.js
+++ source/Model.js
@@ -160,6 +160,10 @@ Model.prototype.signedIn = function (connection) {
   $('#login-button').html(connection.username + ' <i class="fa fa-chevron-down"></i>');
   if (!this.urlUsername || this.urlUsername === connection.username) {// logged into your page
     this.showLoggedInElement();
+    if (this.sharingsConnections.length === 1 &&
+      this.sharingsConnections[0] === this.publicConnection) {
+      this.sharingsConnections = null;
+    }
     if (!this.sharingsConnections) {
       this.addConnection(connection);
       if (this.publicConnection) {

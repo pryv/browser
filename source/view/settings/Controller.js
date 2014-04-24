35f1bf39aae8040743162cdf2f431b94bcbf96b1
jpagand <jordane.pagand@epfl.ch>
2014-04-24 15:40:32 +0200

    v0.6.2 Added Manage Apps panel

--- source/view/settings/Controller.js
+++ source/view/settings/Controller.js
@@ -2,6 +2,7 @@
 var Marionette = require('backbone.marionette'),
   NavView = require('./NavView.js'),
   PasswordView = require('./PasswordView.js'),
+  ManageAppsView = require('./ManageAppsView.js'),
   _ = require('underscore');
 
 var Layout = Marionette.Layout.extend({
@@ -9,7 +10,8 @@ var Layout = Marionette.Layout.extend({
 
   regions: {
     nav: '#settings-nav',
-    password: '#settings-password'
+    password: '#settings-password',
+    manageApps: '#settings-manage-apps'
   },
   initialize: function () {
     this.$el =  $('.modal-content');
@@ -22,12 +24,14 @@ var Controller = module.exports  = function ($modal, connection, target) {
   this.view  = null;
   this.nav = null;
   this.password = null;
+  this.manageApps = null;
   this.currentRegion = '';
 
 
 };
 _.extend(Controller.prototype, {
-  show: function () {
+  show: function (region) {
+    region = region || 'password';
     this.$modal.modal({currentTarget: this.target});
     setTimeout(function () {
       $('.modal-content').fadeIn();
@@ -36,10 +40,11 @@ _.extend(Controller.prototype, {
     this.view.on('close', this.close.bind(this));
     this.nav = new NavView();
     this.password = new PasswordView({connection: this.connection});
+    this.manageApps = new ManageAppsView({connection: this.connection});
     this.view.render();
     this.view.nav.show(this.nav);
-    this._showRegion('password');
-    this.nav.activeRegion('password');
+    this._showRegion(region);
+    this.nav.activeRegion(region);
     this.nav.on('showRegion', this._showRegion.bind(this));
   },
   close: function () {
@@ -49,6 +54,7 @@ _.extend(Controller.prototype, {
       $('#pryv-modal').hide().removeClass('in').attr('aria-hidden', 'true');
       $('.modal-backdrop').remove();
       this.$modal.trigger('hidden.bs.modal');
+      this.manageApps.reset();
     }
   },
   _showRegion: function (region) {
@@ -58,13 +64,18 @@ _.extend(Controller.prototype, {
         case 'password':
           regionView = this.password;
           break;
+        case 'manageApps':
+          regionView = this.manageApps;
+          break;
         default:
           break;
       }
       if (region) {
         this.currentRegion = region;
         this.password.close();
+        this.manageApps.close();
         this.view[region].show(regionView);
+        $('body').i18n();
       }
     }
   }
---
+++ source/view/settings/ManageAppsView.js
@@ -0,0 +1,85 @@
+/* global window, i18n, $*/
+var Marionette = require('backbone.marionette');
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+var GridRow = Marionette.ItemView.extend({
+  template: '#apps-item-settings-template',
+  tagName: 'tr',
+  events: {
+    'click .app-trash': '_onTrashClick'
+  },
+  _onTrashClick: function () {
+    this.trigger('app:delete', this.model);
+  }
+});
+
+
+
+var App = Backbone.Model.extend({});
+
+var AppList = Backbone.Collection.extend({
+  model: App
+});
+
+var allList = new AppList([]);
+
+
+// The grid view
+module.exports = Marionette.CompositeView.extend({
+  tagName: 'div',
+  template: '#apps-list-settings-template',
+  itemView: GridRow,
+  connection: null,
+
+  initialize: function () {
+    this.collection =  this.options.collection || allList;
+    this.listenTo(allList, 'change', this.debounceRender);
+    this.on('itemview:app:delete', this._onDeleteAppClick.bind(this));
+    this.connection = this.options.connection;
+    if (this.connection) {
+      this.connection.accesses.get(function (error, result) {
+        if (error) {
+          window.PryvBrowser.showAlert('.modal-content',
+            i18n.t('error.manageApps.' + error.id));
+        } else {
+          result.forEach(function (access) {
+            if (access.type === 'app') {
+              var m = new App({
+                app: access
+              });
+              allList.add(m);
+            }
+          }.bind(this));
+        }
+      }.bind(this));
+    }
+  },
+  appendHtml: function (collectionView, itemView) {
+    collectionView.$('tbody').append(itemView.el);
+  },
+  _onDeleteAppClick: function (e, model) {
+    this.connection.accesses.delete(model.get('app').id,
+      function (error) {
+        if (!error) {
+          allList.remove(model);
+          this.debounceRender();
+        } else {
+          window.PryvBrowser.showAlert('.modal-content',
+            i18n.t('error.createdSlice.delete.' + error.id));
+          console.warn(error);
+        }
+      }.bind(this));
+  },
+  onRender: function () {
+    $('body').i18n();
+  },
+  reset: function () {
+    this.collection.reset();
+    allList.reset();
+  },
+  debounceRender: _.debounce(function () {
+    this.render();
+  }, 10)
+});
+

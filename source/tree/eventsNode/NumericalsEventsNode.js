889f5c18f079c04e55c0367299b433789ca82db3
jpagand <jordane.pagand@epfl.ch>
2014-08-25 13:44:47 +0200

    Use isNumerical from Pryv lib

--- source/tree/eventsNode/NumericalsEventsNode.js
+++ source/tree/eventsNode/NumericalsEventsNode.js
@@ -23,38 +23,7 @@ var NumericalsEventsNode = module.exports = EventsNode.implement(
 
 // we accept all kind of events
 NumericalsEventsNode.acceptThisEventType = function (eventType) {
-  var eventTypeClass = eventType.split('/')[0];
-  return (
-    eventTypeClass === 'money' ||
-      eventTypeClass === 'absorbed-dose' ||
-      eventTypeClass === 'absorbed-dose-equivalent' ||
-      eventTypeClass === 'absorbed-dose-rate' ||
-      eventTypeClass === 'absorbed-dose-rate' ||
-      eventTypeClass === 'area' ||
-      eventTypeClass === 'capacitance' ||
-      eventTypeClass === 'catalytic-activity' ||
-      eventTypeClass === 'count' ||
-      eventTypeClass === 'data-quantity' ||
-      eventTypeClass === 'density' ||
-      eventTypeClass === 'dynamic-viscosity' ||
-      eventTypeClass === 'electric-charge' ||
-      eventTypeClass === 'electric-charge-line-density' ||
-      eventTypeClass === 'electric-current' ||
-      eventTypeClass === 'electrical-conductivity' ||
-      eventTypeClass === 'electromotive-force' ||
-      eventTypeClass === 'energy' ||
-      eventTypeClass === 'force' ||
-      eventTypeClass === 'length' ||
-      eventTypeClass === 'luminous-intensity' ||
-      eventTypeClass === 'mass' ||
-      eventTypeClass === 'mol' ||
-      eventTypeClass === 'power' ||
-      eventTypeClass === 'pressure' ||
-      eventTypeClass === 'speed' ||
-      eventTypeClass === 'temperature' ||
-      eventTypeClass === 'time' ||
-      eventTypeClass === 'volume'
-    );
+  return window.PryvBrowser.eventTypes.isNumerical(eventType);
 };
 try {
   Object.defineProperty(window.PryvBrowser, 'numericalWeight', {

051701cf6529f2fadb9a268fa5d783c983c0d0ae
jpagand <jordane.pagand@epfl.ch>
2014-10-03 17:21:25 +0200

    Added duration management

--- source/view/events-views/detailed/CommonView.js
+++ source/view/events-views/detailed/CommonView.js
@@ -7,8 +7,7 @@ module.exports = Marionette.ItemView.extend({
   id: 'detail-full',
   // addAttachmentContainer: '#add-attachment',
   waitSubmit: false,
-  // addAttachmentId: 0,
-  // attachmentId: {},
+  timer: null,
   ui: {
     editBtn: '#edit-button',
     editForm: '#edit-form',
@@ -22,20 +21,46 @@ module.exports = Marionette.ItemView.extend({
     editTimePicker: '#edit-time-picker',
     editTags: '#edit-tags',
     editDescription: '#edit-description',
+    addDuration: '#add-duration',
+    stopDuration: '#stop-duration',
+    editDuration: '#edit-duration',
+    removeDuration: '#remove-duration'
   },
   templateHelpers: function () {
     return {
       getStreamStructure: function () {
         return this.getStreamStructure();
+      }.bind(this),
+      getDurationControl: function () {
+        return this.getDurationControl();
+      }.bind(this),
+      displayDuration: function () {
+        return this.displayDuration();
       }.bind(this)
     };
   },
   initialize: function () {
     this.listenTo(this.model, 'change', this.render);
   },
+  onClose: function () {
+    $('.popover-duration').remove();
+    this.stopTimer();
+  },
+  onBeforeRender: function () {
+    $('.popover-duration').remove();
+    this.stopTimer();
+  },
   onRender: function () {
     $(this.itemViewContainer).html(this.el);
-    //  this.addAttachment();
+
+    this.initAddDuration();
+    this.initEditDuration();
+    if (this.ui.removeDuration) {
+      this.ui.removeDuration.bind('click', this.removeDuration.bind(this));
+    }
+    if (this.ui.stopDuration) {
+      this.ui.stopDuration.bind('click', this.stopDuration.bind(this));
+    }
     this.ui.editBtn.bind('click', this.showEdit.bind(this));
     this.ui.editStopEditing.bind('click', this.hideEdit.bind(this));
     this.ui.editForm.bind('submit', this.submit.bind(this));
@@ -103,6 +128,292 @@ module.exports = Marionette.ItemView.extend({
     return result;
 
   },
+  startTimer: function () {
+    this.stopTimer();
+    this.timer = setInterval(function () {
+      if (this.model && this.model.get('event') && this.model.get('event').isRunning()) {
+        var start = moment.unix(this.model.get('event').time);
+        var now = moment();
+        $('.duration-clock').html(moment.preciseDiff(start, now));
+      }
+    }.bind(this), 1000);
+  },
+  stopTimer: function () {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  },
+  /* jshint ignore:start */
+  removeDuration: function () {
+    this.model.get('event').duration = 0;
+    this.ui.saveBtn.trigger('click');
+  },
+  stopDuration: function () {
+    var event = this.model.get('event');
+    if (event.isRunning()) {
+      event.duration = moment().unix() - event.time;
+      this.ui.saveBtn.trigger('click');
+    }
+  },
+  initAddDuration: function () {
+    var that = this;
+    $('#add-duration').popover('destroy');
+    $('#add-duration').popover({
+      html: true,
+      placement: 'top',
+      container: 'body',
+      template: '<div class="popover popover-duration" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+      content: function () {
+        return $('<form class="form-horizontal">' +
+          '<div class="form-group">' +
+          '  <label for="endDatePicker">' + i18n.t('events.common.labels.endDatePicker') + '</label>' +
+          '  <div class="input-group date picker" id="endDatePicker">' +
+          '    <input type="text" class="form-control"/> ' +
+          '    <span id="endDateButton" class="input-group-addon">' +
+          '      <span class="fa fa-calendar"></span>' +
+          '    </span>' +
+          '  </div>' +
+          '</div>' +
+          '<button type="button" id="cancel-add-duration" ' +
+          'class="btn btn-default col-md-4"  style="float: none">' +
+          i18n.t('common.actions.cancel') + '</button>' +
+
+          '<button type="button" id="ok-add-duration" class="btn btn-default col-md-7 col-md-offset-1"' +
+          ' style="float: none">' +
+          i18n.t('common.actions.ok') + '</button>' +
+          '<label class="col-md-5">Or</label>' +
+          '<button type="button" id="start-add-duration" class="btn btn-default col-md-7"' +
+          ' style="float: none">' +
+          i18n.t('common.actions.start') + '</button>' +
+          '</form>').html();
+      }
+    });
+    $('#add-duration').on('hidden.bs.popover', function () {
+      $('.bootstrap-datetimepicker-widget.dropdown-menu').remove();
+    });
+    $('#add-duration').on('shown.bs.popover', function () {
+      var endDate = moment();
+      $(document.body).off('click', '#ok-add-duration');
+      $(document.body).on('click', '#ok-add-duration', function () {
+        endDate = moment(endDate);
+        if (endDate.isValid()) {
+          $('#add-duration').popover('toggle');
+          var event = that.model.get('event');
+          event.duration = endDate.unix() - event.time;
+          that.ui.saveBtn.trigger('click');
+        }
+      });
+      $(document.body).off('click', '#cancel-add-duration');
+      $(document.body).on('click', '#cancel-add-duration', function () {
+        $('#add-duration').popover('toggle');
+      });
+      $(document.body).off('click', '#start-add-duration');
+      $(document.body).on('click', '#start-add-duration', function () {
+        that.model.get('event').duration = null;
+        that.ui.saveBtn.trigger('click');
+        $('#add-duration').popover('toggle');
+      });
+      $(document.body).off('click', '#endDateButton');
+      $(document.body).on('click', '#endDateButton', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+
+      $(document.body).off('click', '#endDatePicker input');
+      $(document.body).on('click', '#endDatePicker input', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+      $('#endDatePicker').datetimepicker({
+        direction: 'auto',
+        language: i18n.lng()
+      });
+      $('#endDatePicker').on('dp.change', function (e) {
+        endDate = e.date;
+      });
+      $('#endDatePicker').data('DateTimePicker').setDate(endDate);
+    });
+  },
+  initAddDuration: function () {
+    var that = this;
+    $('#add-duration').popover('destroy');
+    $('#add-duration').popover({
+      html: true,
+      placement: 'top',
+      container: 'body',
+      template: '<div class="popover popover-duration" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+      content: function () {
+        return $('<form class="form-horizontal">' +
+          '<div class="form-group">' +
+          '  <label for="endDatePicker">' + i18n.t('events.common.labels.endDatePicker') + '</label>' +
+          '  <div class="input-group date picker" id="endDatePicker">' +
+          '    <input type="text" class="form-control"/> ' +
+          '    <span id="endDateButton" class="input-group-addon">' +
+          '      <span class="fa fa-calendar"></span>' +
+          '    </span>' +
+          '  </div>' +
+          '</div>' +
+          '<button type="button" id="cancel-add-duration" ' +
+          'class="btn btn-default col-md-4"  style="float: none">' +
+          i18n.t('common.actions.cancel') + '</button>' +
+
+          '<button type="button" id="ok-add-duration" class="btn btn-default col-md-7 col-md-offset-1"' +
+          ' style="float: none">' +
+          i18n.t('common.actions.ok') + '</button>' +
+          '<label class="col-md-5">Or</label>' +
+          '<button type="button" id="start-add-duration" class="btn btn-default col-md-7"' +
+          ' style="float: none">' +
+          i18n.t('common.actions.start') + '</button>' +
+          '</form>').html();
+      }
+    });
+    $('#add-duration').on('hidden.bs.popover', function () {
+      $('.bootstrap-datetimepicker-widget.dropdown-menu').remove();
+    });
+    $('#add-duration').on('shown.bs.popover', function () {
+      var endDate = moment();
+      $(document.body).off('click', '#ok-add-duration');
+      $(document.body).on('click', '#ok-add-duration', function () {
+        endDate = moment(endDate);
+        if (endDate.isValid()) {
+          $('#add-duration').popover('toggle');
+          var event = that.model.get('event');
+          event.duration = endDate.unix() - event.time;
+          that.ui.saveBtn.trigger('click');
+        }
+      });
+      $(document.body).off('click', '#cancel-add-duration');
+      $(document.body).on('click', '#cancel-add-duration', function () {
+        $('#add-duration').popover('toggle');
+      });
+      $(document.body).off('click', '#start-add-duration');
+      $(document.body).on('click', '#start-add-duration', function () {
+        that.model.get('event').duration = null;
+        that.ui.saveBtn.trigger('click');
+        $('#add-duration').popover('toggle');
+      });
+      $(document.body).off('click', '#endDateButton');
+      $(document.body).on('click', '#endDateButton', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+
+      $(document.body).off('click', '#endDatePicker input');
+      $(document.body).on('click', '#endDatePicker input', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+      $('#endDatePicker').datetimepicker({
+        direction: 'auto',
+        language: i18n.lng()
+      });
+      $('#endDatePicker').on('dp.change', function (e) {
+        endDate = e.date;
+      });
+      $('#endDatePicker').data('DateTimePicker').setDate(endDate);
+    });
+  },
+  initEditDuration: function () {
+    var that = this;
+    $('#edit-duration').popover('destroy');
+    $('#edit-duration').popover({
+      html: true,
+      placement: 'top',
+      container: 'body',
+      template: '<div class="popover popover-duration" role="tooltip"><div class="arrow"></div><div class="popover-content"></div></div>',
+      content: function () {
+        return $('<form class="form-horizontal">' +
+          '<div class="form-group">' +
+          '  <label for="endDatePicker">' + i18n.t('events.common.labels.endDatePicker') + '</label>' +
+          '  <div class="input-group date picker" id="endDatePicker">' +
+          '    <input type="text" class="form-control"/> ' +
+          '    <span id="endDateButton" class="input-group-addon">' +
+          '      <span class="fa fa-calendar"></span>' +
+          '    </span>' +
+          '  </div>' +
+          '</div>' +
+          '<button type="button" id="cancel-add-duration" ' +
+          'class="btn btn-default col-md-4"  style="float: none">' +
+          i18n.t('common.actions.cancel') + '</button>' +
+
+          '<button type="button" id="ok-add-duration" class="btn btn-default col-md-7 col-md-offset-1"' +
+          ' style="float: none">' +
+          i18n.t('common.actions.ok') + '</button>' +
+          '</form>').html();
+      }
+    });
+    $('#edit-duration').on('hidden.bs.popover', function () {
+      $('.bootstrap-datetimepicker-widget.dropdown-menu').remove();
+    });
+    $('#edit-duration').on('shown.bs.popover', function () {
+      var endDate = moment();
+      $(document.body).off('click', '#ok-add-duration');
+      $(document.body).on('click', '#ok-add-duration', function () {
+        endDate = moment(endDate);
+        if (endDate.isValid()) {
+          $('#edit-duration').popover('toggle');
+          var event = that.model.get('event');
+          event.duration = endDate.unix() - event.time;
+          that.ui.saveBtn.trigger('click');
+        }
+      });
+      $(document.body).off('click', '#cancel-add-duration');
+      $(document.body).on('click', '#cancel-add-duration', function () {
+        $('#edit-duration').popover('toggle');
+      });
+      $(document.body).off('click', '#endDateButton');
+      $(document.body).on('click', '#endDateButton', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+
+      $(document.body).off('click', '#endDatePicker input');
+      $(document.body).on('click', '#endDatePicker input', function () {
+        endDate = moment();
+        $('#endDateButton').trigger('click');
+      });
+      $('#endDatePicker').datetimepicker({
+        direction: 'auto',
+        language: i18n.lng()
+      });
+      $('#endDatePicker').on('dp.change', function (e) {
+        endDate = e.date;
+      });
+      $('#endDatePicker').data('DateTimePicker').setDate(endDate);
+    });
+  },
+  displayDuration: function () {
+    var event = this.model.get('event');
+    if (event.isRunning()) {
+      this.startTimer();
+      return '<span class="duration-clock"></span>';
+    } else if (event.duration && event.duration > 0) {
+      return '<span>' +
+        moment.preciseDiff(moment.unix(0), moment.unix(event.duration)) +
+        ' (end: ' + window.PryvBrowser.getTimeString(event.time + event.duration) + ' )</span>';
+    }
+  },
+  getDurationControl: function () {
+    var event = this.model.get('event');
+    if (event.isRunning()) {
+      this.startTimer();
+      return '<span class="duration-clock"></span>' +
+      '<button id="stop-duration" class="btn btn-default" data-i18n="common.actions.stop" type="button"></button>';
+    }
+    if (event.duration && event.duration > 0) {
+      return '<span>' +
+        moment.preciseDiff(moment.unix(0), moment.unix(event.duration)) +
+        ' (end: ' + window.PryvBrowser.getTimeString(event.time + event.duration) + ' )</span>' +
+        '<span class="btn-group">' +
+      '<button class="btn btn-default" id="edit-duration" type="button"><i class="fa fa-calendar"></i></button>' +
+      '<button class="btn btn-danger" id="remove-duration" type="button"><i class="fa fa-times"></i></button>' +
+        '</span>';
+    }
+    if (!event.duration || event.duration <= 0) {
+      return '<button class="btn btn-default" id="add-duration" type="button">Add duration</button>';
+    }
+  },
+  /* jshint ignore:end */
   _walkStreamStructure: function (stream, depth, currentStreamId) {
     var indentNbr = 4,
       result = '<option ';

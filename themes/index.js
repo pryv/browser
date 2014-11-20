3318debd1f6aa65924b584ef567d0baa65e42ab6
Simon Goumaz <simon@attentif.ch>
2014-11-20 13:14:10 +0100

    Browser v0.13.2

---
+++ themes/index.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+
+/**
+ * Theme definitions.
+ * Properties for each theme:
+ * - `id`: must be lowercase and match the theme's folder name
+ * - `appName` (optional): used e.g. to override the page title
+ * - `favicon` (optional): the favicon's relative path from the theme's folder (e.g. "favicon.ico")
+ */
+module.exports = _.reduce([
+  {
+    id: 'domo',
+    appName: 'Domo Safety',
+    favicon: 'favicon.png'
+  }
+], function (map, theme) { map[theme.id] = theme; return map; }, {});
